b9831a62bc50efb3a9dc43fee390ce0817aecf8c
kess4obehi <kessiena4excellence@gmail.com>
2025-12-17 20:06:18 +0100

    added main.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,20 @@
+namespace Subjects {
+  // Declaration merging
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || !this.teacher.experienceTeachingC) {
+        return "No available teacher"
+      }
+
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
